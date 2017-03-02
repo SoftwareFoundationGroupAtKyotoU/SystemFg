22240a42022608b96b7f8df4bbd996b69f00e4d0
Atsushi Igarashi <igarashi@kuis.kyoto-u.ac.jp>
2017-03-03 00:01:09 +0900

    Add position information to RT errors in staged interp.
      Q: CSP of user-defined datatypes doesn't quite work?

--- cogen.ml
+++ cogen.ml
@@ -3,38 +3,46 @@ open Syntax
 open FC
 open Eval
 
+let lift_pos p =
+  let open Lexing in
+  let {pos_fname=fname; pos_lnum=lnum; pos_bol=bol; pos_cnum=cnum} = p in
+  .< {pos_fname=fname; pos_lnum=lnum; pos_bol=bol; pos_cnum=cnum} >.
+
 let rec eval = function
-    Var(p, idx) ->  fun env -> .< lookup idx .~env >.
+    Var(p, idx) -> let p = lift_pos p in fun env -> .< lookup .~p idx .~env >.
   | IConst(_, i) -> fun env -> .< IntV i >.
   | BConst(_, b) -> fun env -> .< BoolV b >.
   | BinOp(p, op, e1, e2) ->
      let v1 = eval e1 in
      let v2 = eval e2 in
+     let p = lift_pos p in
      fun env ->
      .< match op, .~(v1 env), .~(v2 env) with
         Plus, IntV i1, IntV i2 -> IntV (i1 + i2)
       | Mult, IntV i1, IntV i2 -> IntV (i1 * i2)
       | Lt, IntV i1, IntV i2 -> BoolV (i1 < i2)
-      | _ -> err "Can't happen (non-integer argument to binop)">.
+      | _ -> errAt .~p "Can't happen (non-integer argument to binop)">.
   | IfExp(p, e1, e2, e3) ->
      let test = eval e1 in
      let thenclause = eval e2 in
      let elseclause = eval e3 in
+     let p = lift_pos p in
      fun env ->
      .< match .~(test env) with
         BoolV true -> .~(thenclause env)
       | BoolV false -> .~(elseclause env)
-      | _ -> err "Can't happen (nonbool condition)" >.
+      | _ -> errAt .~p "Can't happen (nonbool condition)" >.
   | FunExp (_, id, _, e) ->
      let body = eval e in
      fun env -> .< Fun (fun v -> .~(body .< VB (v, .~env) >.)) >.
   | AppExp (p, e1, e2) ->
      let proc = eval e1 in
      let arg = eval e2 in
+     let p = lift_pos p in
      fun env ->
      .< match .~(proc env) with
           Fun f -> f .~(arg env)
-        | _ -> err "Can't happen (application of nonprocedure value)">.
+        | _ -> errAt .~p "Can't happen (application of nonprocedure value)">.
   | TSFunExp (_, id, e) ->
      let body = eval e in  (**** shift -1 ****)
      fun env -> .< TFun (fun () -> .~(body env)) >.
@@ -43,10 +51,11 @@ let rec eval = function
      fun env -> .< TFun (fun () -> let r = ref () in .~(body .< TB (r, .~env) >.)) >.
   | TAppExp (p, e, _) ->
      let tfun = eval e in
+     let p = lift_pos p in
      fun env ->
      .< match .~(tfun env) with
           TFun f -> f ()
-        | _ -> err "Can't happen (application of non-tyabs)" >.
+        | _ -> errAt .~p "Can't happen (application of non-tyabs)" >.
   | CastExp (p, e, ty1, ty2) ->
      let v = eval e in
      let cast = (ty1 ==> ty2) p Pos in
@@ -62,60 +71,69 @@ and (==>) t1 t2 p plr = match t1, t2 with  (* cast interpretation *)
   | Int, Dyn -> fun env v -> .< Tagged (I, .~v) >.
   | Bool, Dyn -> fun env v -> .< Tagged (B, .~v) >.
   | Arr(Dyn,Dyn), Dyn -> fun env v -> .< Tagged (Ar, .~v) >.
-  | TyVar id, Dyn -> fun env v -> .< Tagged (TV (lookupty id .~env), .~v) >.
+  | TyVar id, Dyn ->
+     let p = lift_pos p in
+     fun env v -> .< Tagged (TV (lookupty .~p id .~env), .~v) >.
   | Dyn, Int ->
-     let msg = errMsg_of_polarity p plr in
+     let msg = errMsg_of_polarity plr in
+     let p = lift_pos p in
      fun env v -> .< match .~v with
                        Tagged(I, v0) -> v0
-                     | Tagged(_, _) -> err msg
-                     | _ -> err "Can't happen (Untagged value)" >.
+                     | Tagged(_, _) -> errAt .~p msg
+                     | _ -> errAt .~p "Can't happen (Untagged value)" >.
   | Dyn, Bool ->
-     let msg = errMsg_of_polarity p plr in
+     let msg = errMsg_of_polarity plr in
+     let p = lift_pos p in
      fun env v -> .< match .~v with
                        Tagged(B, v0) -> v0
-                     | Tagged(_, _) -> err msg
-                     | _ -> err "Can't happen (Untagged value)" >.
+                     | Tagged(_, _) -> errAt .~p msg
+                     | _ -> errAt .~p "Can't happen (Untagged value)" >.
   | Dyn, Arr(Dyn,Dyn) ->
-     let msg = errMsg_of_polarity p plr in
+     let msg = errMsg_of_polarity plr in
+     let p = lift_pos p in
      fun env v -> .< match .~v with
                      | Tagged(Ar, v0) -> v0
-                     | Tagged(_, _) -> err msg
-                     | _ -> err "Can't happen (Untagged value)" >.
+                     | Tagged(_, _) -> errAt .~p msg
+                     | _ -> errAt .~p "Can't happen (Untagged value)" >.
   | Dyn, TyVar id ->
-     let msg = errMsg_of_polarity p plr in
+     let msg = errMsg_of_polarity plr in
+     let p = lift_pos p in
      fun env v -> .< match .~v with
                      | Tagged(TV r, v0) ->
-                        if lookupty id .~env == r then v0
-                        else err msg
-                     | Tagged(_, _) -> err msg
-                     | _ -> err "Can't happen (Untagged value)" >.
+                        if lookupty .~p id .~env == r then v0
+                        else errAt .~p msg
+                     | Tagged(_, _) -> errAt .~p msg
+                     | _ -> errAt .~p "Can't happen (Untagged value)" >.
   | Arr(s1,t1), Arr(s2,t2) ->
      let argcast = (s2 ==> s1) p (neg plr) in
      let rescast = (t1 ==> t2) p plr in
+     let p = lift_pos p in
      fun env v -> .< match .~v with
                        Fun f -> Fun (fun w -> let arg = .~(argcast env .<w>.) in
                                                 .~(rescast env .<f arg>.))
-                     | _ -> err "Can't happen (Non-procedure value)" >.
+                     | _ -> errAt .~p "Can't happen (Non-procedure value)" >.
   | Forall(id1, t1), Forall(id2, t2) ->
      let bodycast = (t1 ==> t2) p plr in
+     let p = lift_pos p in
      fun env v -> .< match .~v with
                        TFun f -> TFun (fun () -> .~(bodycast env .< f () >.))
-                     | _ -> err "Can't happen (Not polyfun)" >.
+                     | _ -> errAt .~p "Can't happen (Not polyfun)" >.
   | ty1, Forall(id2, ty2) ->
      let bodycast = (ty1 ==> ty2) p plr in
      fun env v -> .< TFun (fun () -> .~(bodycast .< TB(ref (), .~env) >. v)) >.
   | Forall(id1, ty1), ty2 ->
      let bodycast = (typeInst ty1 Dyn ==> ty2) p plr in
+     let p = lift_pos p in
      fun env v -> .< match .~v with
                        TFun f -> .~(bodycast env .< f () >.)
-                     | _ -> err "Can't happen (Not polyfun)" >.
+                     | _ -> errAt .~p "Can't happen (Not polyfun)" >.
   | Arr(s1,t1) as ty, Dyn ->
      let cast = (ty ==> Arr(Dyn, Dyn)) p plr in
      fun env v -> .< Tagged (Ar, .~(cast env v)) >.
   | Dyn, (Arr(s, t) as ty) ->
      let cast = (Arr(Dyn,Dyn) ==> ty) p plr in
      fun env v -> cast env .< Tagged (Ar, .~v) >.
-  | _, _ -> err "Can't happen!"
+  | _, _ -> errAt p "Can't happen!"
 
 
 let eval_decl env tyenv = function
